dd0cf10f7ba6bda4d317aff65c7742030308d72d
2000game <felix.kotschenreuther@gmail.com>
2023-02-04 09:57:12 +0100

    Added Error Detection and advanced Logging to script

--- main.py
+++ main.py
@@ -3,6 +3,8 @@ from datetime import datetime
 
 from dateutil.relativedelta import relativedelta
 from notion_client import Client
+from notion_client.errors import APIResponseError
+import logging
 
 secret = str(os.environ["NOTION_SECRET"])
 database_id = (os.environ["NOTION_DATABASE_ID"])
@@ -13,13 +15,13 @@ due_date_filter = {"and": [{"property": "Frequency", "select": {"is_not_empty": 
                            {"property": "Complete", "checkbox": {"equals": True}}]}
 
 recursive_top_level_filter = {"and": [{"property": "Top Level Task", "relation": {"is_empty": True}},
-                                               {"property": "Parent Top Level Task",
-                                                "rollup": {"none": {"relation": {"is_empty": True}}}}]}
+                                      {"property": "Parent Top Level Task",
+                                       "rollup": {"none": {"relation": {"is_empty": True}}}}]}
 
 recursive_top_level_filter_buggy = {"and": [{"property": "Top Level Task", "relation": {"is_empty": True}},
-                                      {"and": [{"property": "Parent Task", "relation": {"is_not_empty": True}},
-                                               {"property": "Parent Top Level Task",
-                                                "rollup": {"none": {"relation": {"is_empty": True}}}}]}]}
+                                            {"and": [{"property": "Parent Task", "relation": {"is_not_empty": True}},
+                                                     {"property": "Parent Top Level Task",
+                                                      "rollup": {"none": {"relation": {"is_empty": True}}}}]}]}
 request_interval_in_seconds = int(os.environ["REQUEST_INTERVAL_IN_SECONDS"])
 
 
@@ -57,7 +59,7 @@ def calculate_new_due_date(task):
 
 
 def is_do_date_empty(task):
-    if task["properties"]["Do Date"]["date"] is None:
+    if task["properties"]["Deadline"]["date"] is None:
         return True
     return False
 
@@ -70,22 +72,29 @@ def is_due_date_empty(task):
 
 def calculate_new_do_date(task):
     frequency = task["properties"]["Frequency"]["select"]["name"]
-    old_do_date = datetime.strptime(task["properties"]["Do Date"]["date"]["start"], "%Y-%m-%d")
+    old_do_date = datetime.strptime(task["properties"]["Deadline"]["date"]["start"], "%Y-%m-%d")
     return update_date_from_frequency(old_do_date, frequency)
 
 
 def update_page_dates(task):
     new_due_date = calculate_new_due_date(task)
-    if is_due_date_empty(task):
-        return
-    elif is_do_date_empty(task):
-        notion.pages.update(page_id=task["id"], properties={"Due Date": {"date": {"start": new_due_date}},
-                                                            "Complete": {"checkbox": False}})
-    else:
-        new_do_date = calculate_new_do_date(task)
-        notion.pages.update(page_id=task["id"], properties={"Due Date": {"date": {"start": new_due_date}},
-                                                            "Do Date": {"date": {"start": new_do_date}},
-                                                            "Complete": {"checkbox": False}})
+    try:
+        if is_due_date_empty(task):
+            return
+        elif is_do_date_empty(task):
+            notion.pages.update(page_id=task["id"], properties={"Due Date": {"date": {"start": new_due_date}},
+                                                                "Complete": {"checkbox": False}})
+        else:
+            new_do_date = calculate_new_do_date(task)
+            notion.pages.update(page_id=task["id"], properties={"Due Date": {"date": {"start": new_due_date}},
+                                                                "Deadline": {"date": {"start": new_do_date}},
+                                                                "Complete": {"checkbox": False}})
+        logging.info("Updated task: " + task["id"])
+    except APIResponseError as e:
+        logging.error(e)
+        logging.error(task["id"])
+        logging.error("Failed to update page dates")
+        raise e
 
 
 def update_top_level_task_field(task):
@@ -94,28 +103,51 @@ def update_top_level_task_field(task):
         for parent_top_level_task in task["properties"]["Parent Top Level Task"]["rollup"]["array"]:
             for relation_field in parent_top_level_task["relation"]:
                 parent_top_level_task_ids.append(relation_field)
-        notion.pages.update(page_id=task["id"], properties={"Top Level Task": {"relation": parent_top_level_task_ids}})
+        try:
+            notion.pages.update(page_id=task["id"],
+                                properties={"Top Level Task": {"relation": parent_top_level_task_ids}})
+            logging.info("Updated top level task field: " + task["id"])
+        except APIResponseError as e:
+            logging.error(e)
+            logging.error(task["id"])
+            logging.error(parent_top_level_task_ids)
+            raise e
 
 
 def update_due_dates():
-    request = notion.databases.query(database_id=database_id, sorts=sorting, filter=due_date_filter)
-    tasks = get_tasks(request)
-    for task in tasks:
-        update_page_dates(task)
+    try:
+        request = notion.databases.query(database_id=database_id, sorts=sorting, filter=due_date_filter)
+        tasks = get_tasks(request)
+        for task in tasks:
+            update_page_dates(task)
+    except APIResponseError as e:
+        logging.error(e)
+        logging.error("Failed to update due dates")
+        raise e
 
 
 def update_top_level_task_dates():
-    request = notion.databases.query(database_id=database_id, sorts=sorting, filter=recursive_top_level_filter)
-    tasks = get_tasks(request)
-    for task in tasks:
-        update_top_level_task_field(task)
+    try:
+        request = notion.databases.query(database_id=database_id, sorts=sorting, filter=recursive_top_level_filter)
+        tasks = get_tasks(request)
+        for task in tasks:
+            update_top_level_task_field(task)
+    except APIResponseError as e:
+        logging.error(e)
+        logging.error("Failed to update top level task dates")
+        raise e
 
 
 def main():
     while True:
-        update_due_dates()
-        update_top_level_task_dates()
-        os.system("sleep {}".format(request_interval_in_seconds))
+        try:
+            update_due_dates()
+            update_top_level_task_dates()
+            os.system("sleep {}".format(request_interval_in_seconds))
+        except APIResponseError as e:
+            logging.error(e)
+            logging.error("Failed to update due dates")
+            os.system("sleep {}".format(request_interval_in_seconds*4))
 
 
 if __name__ == "__main__":
